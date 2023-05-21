d4424d71cd1c5a0c3da621dd6c6705477ef106a2
willianpontes2 <107737768+willianpontes2@users.noreply.github.com>
2023-05-21 08:39:07 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta caneta1 = new Caneta("BIC", "amarela", 0.7f);
+caneta1.status();
+
+Caneta caneta2 = new Caneta("Montblanc", "azul", 0.9f);
+caneta2.status();
+    }
+}
+
+
+
+
+
